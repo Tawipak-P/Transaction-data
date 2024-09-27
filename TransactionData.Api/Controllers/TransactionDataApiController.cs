b872929608b05772fc70d1e59f5d5ff5c34ecbcf
Tawipak-P <tawipak.p@gmail.com>
2024-09-27 23:20:30 +0700

    Create Api project

---
+++ TransactionData.Api/Controllers/TransactionDataApiController.cs
@@ -0,0 +1,41 @@
+﻿using Azure;
+using Microsoft.AspNetCore.Mvc;
+using Transaction.Core.Models;
+using Transaction.Core.Services.Interfaces;
+using TransactionData.Core.DTO;
+
+namespace TransactionData.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionDataApiController : ControllerBase
+    {
+        private readonly ITransactionService _transactionService;
+        public TransactionDataApiController(ITransactionService transactionService)
+        {
+            _transactionService = transactionService;
+        }
+
+
+        [HttpPost("transaction-data")]
+        public async Task<IActionResult> GetTransactionData(SearchCriteria searchCriteria)
+        {
+            ResponseModel response = new ResponseModel();
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest();
+                }
+                response = await _transactionService.SearchTransactionDataAsync(searchCriteria);
+                return Ok(response);
+            }
+            catch(Exception ex)
+            {
+                response.IsSuccess = false;
+                response.ErrorMessages = new List<string> { ex.Message };
+                return BadRequest(response);
+            }
+        }
+    }
+}
---
+++ TransactionData.Api/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.EntityFrameworkCore;
+using Transaction.Core.Services.Interfaces;
+using Transaction.Core.Services;
+using Transaction.Infrastructor.Repositories.Interfaces;
+using Transaction.Infrastructor.Repositories;
+using Transaction.Infrastructor;
+using Transaction.Core.MappingConfig;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddDbContext<TransactionDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("TransactionDb"));
+});
+
+
+builder.Services.AddScoped<ITransactionService, TransactionService>();
+builder.Services.AddScoped<ITransactionRepository, TransactionRepository>();
+
+builder.Services.AddAutoMapper(typeof(MappingConfig));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
