503a3b8faa175a7954b34b4615e81a59cb2706c8
Tawipak-P <tawipak.p@gmail.com>
2024-09-27 23:23:19 +0700

    Update and Add new DbContext

---
+++ Transaction.Infrastructor/DbContext/TempTransactionDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Transaction.Infrastructor.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Transaction.Infrastructor
+{
+    public class TempTransactionDbContext : DbContext
+    {
+        public TempTransactionDbContext(DbContextOptions<TempTransactionDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<TM_Transaction> TM_TransactionData {  get; set; }
+
+    }
+}
--- Transaction.Infrastructor/DbContext/TransactionDbContext.cs
+++ Transaction.Infrastructor/DbContext/TransactionDbContext.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using _2C2P.AssignmentTest.Infrastructor.Entities;
 
 namespace Transaction.Infrastructor
 {
@@ -15,7 +16,26 @@ namespace Transaction.Infrastructor
             
         }
 
-        public DbSet<TransactionData> TD_TransactionData {  get; set; }
+        public DbSet<TD_Transaction> TD_Transactions {  get; set; }
+        public DbSet<TD_Status> TD_Statuses { get; set; }
+
+        
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TransactionDataResults>().HasNoKey();
+
+            modelBuilder.Entity<TD_Status>().HasData(new List<TD_Status>
+            {
+                new TD_Status() { Id = 1, Name = "Approved", Prefix = "A" },
+                new TD_Status() { Id = 2, Name = "Rejected", Prefix = "R" },
+                new TD_Status() { Id = 3, Name = "Done", Prefix = "D" },
+                new TD_Status() { Id = 4, Name = "Failed", Prefix = "R" },
+                new TD_Status() { Id = 5, Name = "Finished", Prefix = "D" }
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
 
     }
 }
