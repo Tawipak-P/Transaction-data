c31db2f408dcca6ee0302a5fd5acbbef738a3a1d
Tawipak-P <tawipak.p@gmail.com>
2024-09-28 04:45:12 +0700

    Add store procedures results files

---
+++ Transaction.Infrastructor/StoreProcedures/InsertTransactionDataResults.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transaction.Infrastructor.StoreProcedures
+{
+    public class InsertTransactionDataResults
+    {
+        public int TotalRecords { get; set; }
+     }
+}
---
+++ Transaction.Infrastructor/StoreProcedures/TransactionDataResults.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Transaction.Infrastructor.StoreProcedures
+{ 
+    public class TransactionDataResults
+    {
+        public string Id { get; set; }
+        public string Payment { get; set; }
+        public string Status { get; set; }
+     }
+}
