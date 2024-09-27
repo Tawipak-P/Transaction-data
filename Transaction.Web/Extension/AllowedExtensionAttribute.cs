e7a36de144bc8c45986d0f95c5ce5289cd4fec26
Tawipak-P <tawipak.p@gmail.com>
2024-09-28 04:50:43 +0700

    Create custom validation

---
+++ Transaction.Web/Extension/AllowedExtensionAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Transaction.Extension
+{
+    public class AllowedExtensionAttribute : ValidationAttribute
+    {
+        private readonly string[] _extensions;
+
+        public AllowedExtensionAttribute(string[] extensions)
+        {
+            _extensions = extensions;
+        }
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var file = value as IFormFile;
+            if (file != null)
+            {
+                var extension = Path.GetExtension(file.FileName).ToLower();
+                if (!_extensions.Contains(extension))
+                {
+                    return new ValidationResult("Unknown format.");
+                }
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
