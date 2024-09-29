ec1f7501671682006d95bc294f9faf9a5c4fc634
Tawipak-P <tawipak.p@gmail.com>
2024-09-29 16:37:20 +0700

    Update upload file method

--- Transaction.Core/Services/Interfaces/ITempTransactionService.cs
+++ Transaction.Core/Services/Interfaces/ITempTransactionService.cs
@@ -1,12 +1,14 @@
-﻿using System.Data;
+﻿using Microsoft.AspNetCore.Http;
+using System.Data;
 using System.Xml.Linq;
+using Transaction.Core.DTO;
 using Transaction.Core.Models;
 
 namespace Transaction.Core.Services.Interfaces
 {
     public interface ITempTransactionService
     {
-        Task<ResponseModel> UploadTransactionDataFromCSVAsync(DataTable dataTable);
-        Task<ResponseModel> UploadTransactionDataFromXMLAsync(XDocument xDocument);
+        Task<ResponseModel> UploadTransactionDataFromCSVAsync(FileUploadDTO file);
+        Task<ResponseModel> UploadTransactionDataFromXMLAsync(FileUploadDTO file);
     }
 }
--- Transaction.Core/Services/TempTransactionService.cs
+++ Transaction.Core/Services/TempTransactionService.cs
@@ -1,7 +1,13 @@
 ﻿using AutoMapper;
+using CsvHelper;
+using CsvHelper.Configuration;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using System.Data;
+using System.Formats.Asn1;
+using System.Globalization;
 using System.Xml.Linq;
+using Transaction.Core.DTO;
 using Transaction.Core.Models;
 using Transaction.Core.Services.Interfaces;
 using Transaction.Infrastructor.Repositories.Interfaces;
@@ -11,49 +17,114 @@ namespace Transaction.Core.Services
     public class TempTransactionService : ITempTransactionService
     {
         private readonly ITempTransactionRepository _tempTransactionRepository;
-        private readonly IMapper _mapper;
-        private readonly ILogger<TempTransactionService> _logger;
-        public TempTransactionService(ITempTransactionRepository tempTransactionRepository, IMapper mapper, ILogger<TempTransactionService> logger)
+        public TempTransactionService(ITempTransactionRepository tempTransactionRepository)
         {
             _tempTransactionRepository = tempTransactionRepository;
-            _mapper = mapper;
-            _logger = logger;
         }
-        public async Task<ResponseModel> UploadTransactionDataFromCSVAsync(DataTable dataTable)
+
+
+
+        public async Task<ResponseModel> UploadTransactionDataFromCSVAsync(FileUploadDTO file)
         {
             ResponseModel response = new ResponseModel();
             try
             {
+                var dataTable = ConvertCSVToDataTable(file.TransactionFile);
                 var results = await _tempTransactionRepository.UploadTransactionDataFormCSVAsync(dataTable);
-                response = _mapper.Map<ResponseModel>(results);
+                if (!results)
+                {
+                    response.IsSuccess = false;
+                }
                 return response;
             }
             catch (Exception ex)
             {
-                _logger?.LogError(ex.ToString());
-                response.IsSuccess = false;
-                response.ErrorMessages = new List<string> { ex.Message };
-                return response;
+                throw ex;
             }
         }
 
 
-        public async Task<ResponseModel> UploadTransactionDataFromXMLAsync(XDocument xDocument)
+        public async Task<ResponseModel> UploadTransactionDataFromXMLAsync(FileUploadDTO file)
         {
             ResponseModel response = new ResponseModel();
             try
             {
+                var xDocument = ConvertXmlToXDocument(file.TransactionFile);
                 var results = await _tempTransactionRepository.UploadTransactionDataFromXMLAsync(xDocument);
-                response = _mapper.Map<ResponseModel>(results);
+                if (!results)
+                {
+                    response.IsSuccess = false;
+                }
+
                 return response;
             }
             catch (Exception ex)
             {
-                _logger?.LogError(ex.ToString());
-                response.IsSuccess = false;
-                response.ErrorMessages = new List<string> { ex.Message };
-                return response;
+                throw ex;
             }
         }
+
+
+
+        private DataTable ConvertCSVToDataTable(IFormFile file)
+        {
+            var dataTable = new DataTable();
+            try
+            {
+                var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+                {
+                    HasHeaderRecord = false,
+                };
+                using (var streamReader = new StreamReader(file.OpenReadStream()))
+                {
+                    using (var csv = new CsvReader(streamReader, config))
+                    {
+                        csv.Read();
+                        var dataReader = new CsvDataReader(csv);
+                        dataTable.Load(dataReader);
+                    }
+                }
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private XDocument ConvertXmlToXDocument(IFormFile file)
+        {
+            var extension = Path.GetExtension(file.FileName);
+            var fileName = file.FileName.Split('.')[0] + '_' + DateTime.Now.Millisecond + extension;
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads/tempXmlFile", fileName);
+            try
+            {
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+                var xDocument = XDocument.Load(filePath);
+
+                //clear file
+                if (!String.IsNullOrEmpty(xDocument.ToString()))
+                {
+                    if (System.IO.File.Exists(filePath))
+                    {
+                        System.IO.File.Delete(filePath);
+                    }
+                }
+                return xDocument;
+            }
+            catch (Exception ex)
+            {
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+
+                throw ex;
+            }
+        }
+
     }
 }
