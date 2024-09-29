9ae5df9e14e0316e43fc2b608d4447284ba0d82e
Tawipak-P <tawipak.p@gmail.com>
2024-09-29 17:23:52 +0700

    Update database

---
+++ Transaction.Infrastructor/Migrations/20240929101627_DropCurrecyTable.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Transaction.Infrastructor.Migrations.TransactionDb
+{
+    /// <inheritdoc />
+    public partial class DropCurrecyTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TD_CurrencyCodes");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TD_CurrencyCodes",
+                columns: table => new
+                {
+                    CurrencyCode = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    Id = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TD_CurrencyCodes", x => x.CurrencyCode);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TD_CurrencyCodes",
+                columns: new[] { "CurrencyCode", "Id" },
+                values: new object[,]
+                {
+                    { "AUD", 5 },
+                    { "CAD", 6 },
+                    { "CHF", 7 },
+                    { "CNY", 8 },
+                    { "EUR", 2 },
+                    { "GBP", 4 },
+                    { "JPY", 3 },
+                    { "NZD", 10 },
+                    { "SEK", 9 },
+                    { "USD", 1 }
+                });
+        }
+    }
+}
---
+++ Transaction.Infrastructor/Migrations/20240929101659_AddNewCurrecyTable2.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Transaction.Infrastructor.Migrations.TransactionDb
+{
+    /// <inheritdoc />
+    public partial class AddNewCurrecyTable2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TD_CurrencyCodes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CurrencyCode = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TD_CurrencyCodes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TD_CurrencyCodes",
+                columns: new[] { "Id", "CurrencyCode" },
+                values: new object[,]
+                {
+                    { 1, "USD" },
+                    { 2, "EUR" },
+                    { 3, "JPY" },
+                    { 4, "GBP" },
+                    { 5, "AUD" },
+                    { 6, "CAD" },
+                    { 7, "CHF" },
+                    { 8, "CNY" },
+                    { 9, "SEK" },
+                    { 10, "NZD" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TD_CurrencyCodes");
+        }
+    }
+}
