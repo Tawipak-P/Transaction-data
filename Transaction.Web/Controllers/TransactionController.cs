86a08a87d54eec0233511ebca228c4156f3a8348
Tawipak-P <tawipak.p@gmail.com>
2024-09-29 16:39:41 +0700

    Update controller class

--- Transaction.Web/Controllers/TransactionController.cs
+++ Transaction.Web/Controllers/TransactionController.cs
@@ -1,16 +1,10 @@
 ﻿using Transaction.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 using Transaction.Core.Services.Interfaces;
-using Transaction.Core.Models;
-using CsvHelper;
-using System.Globalization;
-using System.Data;
-using CsvHelper.Configuration;
-using System.Xml;
 using AutoMapper;
-using System.Xml.Linq;
-using Serilog.Core;
 using Serilog;
+using Transaction.Core.Models;
+using Transaction.Core.DTO;
 
 namespace Transaction.Web.Controllers
 {
@@ -18,6 +12,7 @@ namespace Transaction.Web.Controllers
     {
         private readonly ITempTransactionService _tempTransactionService;
         private readonly IMapper _mapper;
+
         public TransactionController(ITempTransactionService tempTransactionService, IMapper mapper)
         {
             _tempTransactionService = tempTransactionService;
@@ -44,88 +39,36 @@ namespace Transaction.Web.Controllers
                     return View(fileUploadModel);
                 }
 
+                var file = _mapper.Map<FileUploadDTO>(fileUploadModel);
                 switch (Path.GetExtension(fileUploadModel.TransactionFile.FileName))
                 {
                     case ".csv":
-                        var dataTable = ConvertCSVToDataTable(fileUploadModel.TransactionFile);
-                        response = await _tempTransactionService.UploadTransactionDataFromCSVAsync(dataTable);
+                        response = await _tempTransactionService.UploadTransactionDataFromCSVAsync(file);
                         break;
                     default:
-                        var xDocument = ConvertXmlToXDocument(fileUploadModel.TransactionFile);
-                        response = await _tempTransactionService.UploadTransactionDataFromXMLAsync(xDocument);
+                        response = await _tempTransactionService.UploadTransactionDataFromXMLAsync(file);
                         break;
                 }
 
-                if (response.IsSuccess)
+                if (!response.IsSuccess)
                 {
-                    TempData["success"] = "Uploaded successfully";
+                    TempData["error"] = "Unsuccess to upload transaction data";
+                    fileUploadModel.FileName = fileUploadModel.TransactionFile.FileName;
+                    return View(fileUploadModel);
                 }
-
+                
+                TempData["success"] = "Uploaded successfully";
                 ModelState.Clear();
-                return View(fileUploadModel);
+                return View();
             }
             catch (Exception ex)
             {
                 response.IsSuccess = false;
                 TempData["error"] = "Unsuccess to upload transaction data";
                 Log.Error(ex.ToString());
-                ModelState.Clear();
-                return View();
+                fileUploadModel.FileName = fileUploadModel.TransactionFile.FileName;
+                return View(fileUploadModel);
             }
         }
-
-        private DataTable ConvertCSVToDataTable(IFormFile file)
-        {
-            var dataTable = new DataTable();
-
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = false,
-            };
-            using (var streamReader = new StreamReader(file.OpenReadStream()))
-            {
-                using (var csv = new CsvReader(streamReader, config))
-                {
-                    csv.Read();
-                    var dataReader = new CsvDataReader(csv);
-                    dataTable.Load(dataReader);
-                }
-            }
-            return dataTable;
-        }
-        private XDocument ConvertXmlToXDocument(IFormFile file)
-        {
-            var extension = Path.GetExtension(file.FileName);
-            var fileName = file.FileName.Split('\\')[0] + '_' + DateTime.Now.Millisecond + extension;
-            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/uploads/xml", fileName);
-            try
-            {
-                using (var fileStream = new FileStream(filePath, FileMode.Create))
-                {
-                    file.CopyTo(fileStream);
-                }
-                var xDocument = XDocument.Load(filePath);
-
-                //clear file
-                if (!String.IsNullOrEmpty(xDocument.ToString()))
-                {
-                    if (System.IO.File.Exists(filePath))
-                    {
-                        System.IO.File.Delete(filePath);
-                    }
-                }
-                return xDocument;
-            }
-            catch (Exception ex)
-            {
-                if (System.IO.File.Exists(filePath))
-                {
-                    System.IO.File.Delete(filePath);
-                }
-
-                throw ex;
-            } 
-        }
-
     }
 }
---
+++ Transaction.Web/MappingConfig/MappingConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Transaction.Core.DTO;
+using Transaction.Web.Models;
+
+namespace Transaction.Web.MappingConfig
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<FileUploadModel, FileUploadDTO>().ReverseMap();
+        }
+    }
+}
